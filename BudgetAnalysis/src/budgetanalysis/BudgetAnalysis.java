ba73e5c62c026905bfb10e78ac8d5594ccb896dc
Peng Chen <pc8w3@mail.missouri.edu>
2015-07-12 17:33:09 -0500

    create project using NetBeans

---
+++ BudgetAnalysis/src/budgetanalysis/BudgetAnalysis.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package budgetanalysis;
+
+/**
+ *
+ * @author pc8w3_000
+ */
+public class BudgetAnalysis {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
